213ca1f379c7c6b0542f60fa9b50c02cafb9e83d
Radek <radek.halfaf@gmail.com>
2020-06-30 21:38:08 +0200

    create Question repository

--- src/main/java/elerning/Controller/Questions/QuestionController.java
+++ src/main/java/elerning/Controller/Questions/QuestionController.java
@@ -2,10 +2,17 @@ package elerning.Controller.Questions;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/quiz")
 @Controller
 public class QuestionController {
 
+    @GetMapping("/displayQuestion")
+    private String displayQuestion() {
+        return "displayQuestion";
+    }
 
 
 }
