abfbd8cfa5608dd1fc11a26aacdf6fa3ed06eb45
Radek <radek.halfaf@gmail.com>
2020-07-11 21:36:51 +0200

    postgres implementation

--- src/main/java/elerning/Configuration/WebSecurityConfig.java
+++ src/main/java/elerning/Configuration/WebSecurityConfig.java
@@ -38,6 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(
                         "/login",
+                        "/save",
                         "/quiz/**",
                         "/",
                         "/home",
