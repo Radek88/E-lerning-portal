aaaecea9a4e7f70d3f98e25ed1c142a7b5ed0519
rhalfaf <58357474+rhalfaf@users.noreply.github.com>
2020-07-28 19:04:48 +0200

    Update EmailServiceImplementation.java

--- src/main/java/elerning/Service/Email/EmailServiceImplementation.java
+++ src/main/java/elerning/Service/Email/EmailServiceImplementation.java
@@ -14,7 +14,7 @@ public class EmailServiceImplementation implements EmailService {
     public void sendEmail(String senderName, String senderEmail, String subject, String messagePlainText) {
 
         final String username = "elerningproject@gmail.com";
-        final String password = "${{ secrets.DB_PASS }}";
+        final String password = "${{ secrets.EMAIL_PASSWORD }}";
 
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
